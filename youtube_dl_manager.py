f760fd8909b3d50a7b46b8a66bf22949107b2f1b
Dmitry Borzenin <fked9181@yahoo.com>
2019-09-10 17:57:04 +0300

    YoutubeManager: add get_info implementation
    gets video's title, format list, duration, filesize

--- youtube_dl_manager.py
+++ youtube_dl_manager.py
@@ -1,7 +1,9 @@
 import asyncio
 import logging
 from concurrent.futures import ThreadPoolExecutor
-from typing import Union
+from typing import Union, Optional
+
+from youtube_dl import YoutubeDL
 
 
 class YoutubeManager:
@@ -27,3 +29,45 @@ class YoutubeManager:
             self._downloads.remove(str(id_))
         except ValueError:
             pass
+
+    async def get_info(self, url: str) -> Optional[dict]:
+        future = self.loop.create_future()
+        self.loop.run_in_executor(self.executor, self._get_info, url, future, self.logger)
+        return await future
+
+    @staticmethod
+    def _get_info(url: str, future: asyncio.Future, logger: logging.Logger):
+        try:
+            with YoutubeDL({'logger': logger}) as ydl:
+                data = ydl.extract_info(url, download=False)
+                title = data.get('title', 'No title')
+                duration = data.get('duration', 0)
+                formats = data.get('formats', None)
+
+                good_formats = {}
+                for format in formats:
+                    format_id = format.get('format_id', None)
+                    ext = format.get('ext', None)
+                    format_note = format.get('format_note', None)
+                    filesize = format.get('filesize', None)
+                    vcodec = format.get('vcodec', None)
+
+                    if ext != 'mp4' or format_id is None or format_note is None or filesize is None:
+                        continue
+
+                    if not vcodec.startswith('avc1'):
+                        continue
+
+                    if not format_note.endswith('p') or not format_note[:-1].isdigit():
+                        continue
+
+                    if good_formats.get(format_note, None) is not None and good_formats[format_note]['filesize'] > int(
+                            filesize):
+                        continue
+
+                    good_formats[format_note] = {'format_id': format_id, 'filesize': int(filesize)}
+
+            future.set_result({'title': title, 'formats': good_formats, 'duration': duration})
+
+        except Exception:
+            future.set_result(None)
