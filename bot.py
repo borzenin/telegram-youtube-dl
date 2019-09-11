dcc123fce2ac2839b1bbe7d2c7bb86b6d764b9da
Dmitry Borzenin <fked9181@yahoo.com>
2019-09-11 09:46:48 +0300

    YoutubeDownloaderBot: add on_download_type_picked handler
    on_download_type_picked implementation
    on_type_video_picked implementation

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import asyncio
 import json
 import logging
+from math import ceil
 from typing import Sequence
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from telethon import Button
 from telethon import TelegramClient
-from telethon.events import NewMessage
+from telethon.events import NewMessage, CallbackQuery
 from telethon.sessions import StringSession
 
 from bot_events import Handler
@@ -134,3 +135,55 @@ class YoutubeDownloaderBot:
             Button.inline('Audio (default)', data=f'type_{download_id}_{DownloadType.AUDIO_DEFAULT.name}'),
             Button.inline('Audio (custom)', data=f'type_{download_id}_{DownloadType.AUDIO_CUSTOM.name}')
         ])
+
+    @Handler.register(CallbackQuery(pattern=rb'^type_'))
+    async def on_download_type_picked(self, event: CallbackQuery.Event):
+        download_type = '_'.join(event.data.decode().split('_')[2:])
+        self.logger.debug('Download type was picked: {}'.format(download_type))
+        await event.delete()
+
+        if download_type == DownloadType.VIDEO.name:
+            return await self.on_type_video_picked(event)
+        elif download_type == DownloadType.AUDIO_DEFAULT.name:
+            return await self.on_type_audio_default_picked(event)
+        elif download_type == DownloadType.AUDIO_CUSTOM.name:
+            return await self.on_type_audio_custom_picked(event)
+
+        self.logger.error('Invalid type was received: {}'.format(download_type))
+
+    async def on_type_video_picked(self, event: CallbackQuery.Event):
+        download_id = event.data.decode().split('_')[1]
+        with use_session(self.session_cls) as db_session:
+            download = db_session.query(Download).filter(Download.chat_id == event.chat_id).one_or_none()
+
+        if download is None or download.download_id != download_id:
+            self.logger.error('Download object was not exists. Skip.')
+            await event.delete()
+            return
+
+        formats = sorted(
+            json.loads(download.video_format_list).items(),
+            key=lambda x: int(x[0][:-1])
+        )
+
+        # matrix generation
+        buttons = [
+            [
+                Button.inline(
+                    '{} ({} Mb)'.format(key, round(value['filesize'] / 1000000, 1)),
+                    data='video_{}_{}'.format(download_id, value['format_id'])
+                )
+                for key, value in formats[i * 3: (i + 1) * 3]
+            ]
+            for i in range(ceil(len(formats) / 3))
+        ]
+        await event.respond(
+            '<b>{}</b>\nChoose quality (if less then 10 Mb, GIF will be sent): '.format(download.title),
+            buttons=buttons
+        )
+
+    async def on_type_audio_default_picked(self, event: NewMessage.Event):
+        pass
+
+    async def on_type_audio_custom_picked(self, event: NewMessage.Event):
+        pass
